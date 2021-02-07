76bb842a08013d6ccfeb80955eb1fb3ddf0bd1f8
N-bred <thehardyboiz1@gmail.com>
2021-02-06 19:28:02 -0500

    Added a more scalable solution to treasureIsland

--- treasureIsland/main.py
+++ treasureIsland/main.py
@@ -1,28 +1,47 @@
-print("Welcome to Treasure Island")
+questions = {
+    "question": "You're at a cross road. Where do you want to go? Type 'left' or 'right': ",
+    "options": {
+        "left": {
+            "question": "You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across: ",
+            "options": {
+                "wait": {
+                    "question": "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?: ",
+                    "options": {
+                        "red": {"gameover": "It's a room full of fire. Game Over"},
+                        "yellow": {"gameover": "It's a room full of gold, good job"},
+                        "blue": {
+                            "gameover": "it's a room full of water, you drown. Game Over"
+                        },
+                    },
+                },
+                "swim": {
+                    "gameover": "You try to swim but its a big body of water and you end up drowning, Game Over"
+                },
+            },
+        },
+        "right": {"gameover": "There's only emptyness on the right, Game Over"},
+    },
+}
 
-print("Your mission is to find the treasure")
 
-direction = input(
-    "You're at a cross road. Where do you want to go? Type 'left' or 'right': "
-)
+def make_question(question):
+    if "gameover" in question:
+        print(question["gameover"])
+        return
 
-if direction == "left":
-    action = input(
-        "You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across: "
-    )
-    if action == "wait":
-        a = input(
-            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?: "
-        )
-        if a == "red":
-            print("It's a room full of fire. Game Over")
-        elif a == "yellow":
-            print("It's a room full of gold, good job")
-        elif a == "blue":
-            print("it's a room full of water, you drown. Game Over")
-    elif action == "swim":
-        print(
-            "You try to swim but its a big body of water and you end up drowning, Game Over"
-        )
-elif direction == "right":
-    print("There's only emptyness on the right, Game Over")
+    answer = input(question["question"])
+    if answer in question["options"]:
+        return make_question(question["options"][answer])
+    else:
+        print("bad answer")
+        return make_question(question)
+
+
+def main():
+    if __name__ == "__main__":
+        print("Welcome to Treasure Island")
+        print("Your mission is to find the treasure")
+        make_question(questions)
+
+
+main()
