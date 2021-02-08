2db730354b5788d099480deed7e5635c45b180a6
N-bred <thehardyboiz1@gmail.com>
2021-02-08 16:58:19 -0500

    Finished second version of the solution, now more concise and with upper case allowance

--- passwordGenerator/main.py
+++ passwordGenerator/main.py
@@ -1,57 +1,40 @@
 import random
-
-print("Welcome to the password generator")
-
-n_of_chars = int(input("How many letters long: "))
-n_of_symbols = int(input("How many symbols do you want: "))
-n_of_numbers = int(input("How many numbers do you want: "))
+from string import ascii_lowercase, ascii_uppercase
 
 
-password = ""
+def main():
+    if __name__ == "__main__":
+        print("Welcome to the password generator")
+        n_of_chars = int(input("How many letters long: "))
+        n_of_symbols = int(input("How many symbols do you want: "))
+        n_of_numbers = int(input("How many numbers do you want: "))
+        allow_uppercase = int(
+            input("Allow uppercase letters? press 1 to allow or 0 to avoid: ")
+        )
+
+        numbers = [n for n in range(0, 9)]
+        symbols = ["$", "%", "#", "!", "&", "*", "_", "~"]
+        password = ""
+
+        for n in range(0, n_of_numbers):
+            password += str(random.choice(numbers))
+
+        for n in range(0, n_of_symbols):
+            password += random.choice(symbols)
+
+        for n in range(len(password), n_of_chars):
+            if allow_uppercase == 1:
+                if random.random() > 0.5:
+                    password += random.choice(ascii_uppercase)
+                else:
+                    password += random.choice(ascii_lowercase)
+            else:
+                password += random.choice(ascii_lowercase)
+
+        randomized_password = list(password)
+        random.shuffle(randomized_password)
+        randomized_password = "".join(randomized_password)
+        return print(f"Your password is: {randomized_password}")
 
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-symbols = ["$", "%", "#", "!", "&", "*", "_", "~"]
-chars = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-]
-
-
-for n in range(0, n_of_numbers):
-    password += str(random.choice(numbers))
-
-for n in range(0, n_of_symbols):
-    password += random.choice(symbols)
-
-for n in range(len(password), n_of_chars):
-    password += random.choice(chars)
-
-randomized_password = list(password)
-random.shuffle(randomized_password)
-randomized_password = "".join(randomized_password)
-
-print(f"Your password is: {randomized_password}")
+main()
