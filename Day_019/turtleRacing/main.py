3f40e681334e21b8a9f33644675693cbba17a30e
N-bred <thehardyboiz1@gmail.com>
2021-02-23 18:32:36 -0500

    Refactored coded

--- Day_019/turtleRacing/main.py
+++ Day_019/turtleRacing/main.py
@@ -1,47 +1,51 @@
 from turtle import Turtle, Screen
 import random
 
-screen = Screen()
-
-width, height = 500, 400
-
-screen.setup(width, height)
-color_to_win = screen.textinput(
-    title="Make your bet", prompt="Which turtle will win the race? Enter a color: "
-)
-
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-turtles = []
+WIDTH, HEIGHT = 500, 400
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 
-def draw_turtles():
+def get_colors(colors):
+    return "\n- ".join(colors)
+
+
+def draw_turtles(colors):
+    turtles = []
     for i, color in list(enumerate(colors)):
         tim = Turtle(shape="turtle")
         tim.color(color)
         tim.penup()
         tim.goto(x=-230, y=-100 + i * 40)
         turtles.append(tim)
+    return turtles
 
 
-draw_turtles()
-
-
-def declare_winner(turtle):
-    if turtle.color()[0] == color_to_win:
+def declare_winner(turtle, win_color):
+    if turtle.color()[0] == win_color:
         print("Congratulations, you win")
     else:
         print(f"You lose lol, the winner was: {turtle.color()[0]}")
 
 
-is_running = True
-while is_running:
-    for turtle in turtles:
-        turtle.forward(random.randint(0, 15))
+def main():
+    if __name__ == "__main__":
+        screen = Screen()
+        screen.setup(WIDTH, HEIGHT)
+        chose_color = screen.textinput(
+            title="Make your bet",
+            prompt=f"Which turtle will win the race? Enter a color ({get_colors(COLORS)}): ",
+        )
+        turtles = draw_turtles(COLORS)
+        is_running = True
+        while is_running:
+            for turtle in turtles:
+                turtle.forward(random.randint(0, 15))
 
-        if turtle.pos()[0] >= width / 2:
-            declare_winner(turtle)
-            is_running = False
-            break
+                if turtle.pos()[0] >= WIDTH / 2:
+                    declare_winner(turtle, chose_color)
+                    is_running = False
+                    break
+        screen.exitonclick()
 
 
-screen.exitonclick()
+main()
