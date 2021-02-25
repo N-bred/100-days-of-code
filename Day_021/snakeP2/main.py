1e45621b38909e2895b272e86c661df6e7e13199
N-bred <thehardyboiz1@gmail.com>
2021-02-25 03:04:29 -0500

    Added food to the project

--- Day_021/snakeP2/main.py
+++ Day_021/snakeP2/main.py
@@ -1,9 +1,9 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 WIDTH, HEIGHT = 600, 600
-
 
 screen = Screen()
 screen.setup(WIDTH, HEIGHT)
@@ -13,6 +13,8 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,8 +26,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.05)
-
     snake.move_snake()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
