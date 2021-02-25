35d91b0c9601baac97ade41bca431a0eb84d788d
N-bred <thehardyboiz1@gmail.com>
2021-02-25 03:16:56 -0500

    Created the scoreboard class

--- Day_021/snakeP2/main.py
+++ Day_021/snakeP2/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 WIDTH, HEIGHT = 600, 600
@@ -14,7 +15,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -30,6 +31,8 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+        scoreboard.write_score()
 
 
 screen.exitonclick()
