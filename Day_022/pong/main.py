aad05c00fa156320f62f3e0159e2c4def5bc80bc
N-bred <thehardyboiz1@gmail.com>
2021-02-26 16:47:49 -0500

    Added screen

---
+++ Day_022/pong/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+WIDTH, HEIGHT = 800, 600
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=WIDTH, height=HEIGHT)
+screen.title("PONG")
+
+
+screen.exitonclick()
