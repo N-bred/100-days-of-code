65bc1d4314d109bd721300eeeed80c7317ca6bd1
N-bred <thehardyboiz1@gmail.com>
2021-02-26 17:38:43 -0500

    Added scoreboard class with definitions

--- Day_022/pong/main.py
+++ Day_022/pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 WIDTH, HEIGHT = 800, 600
@@ -15,6 +16,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -42,9 +45,11 @@ while game_is_on:
 
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
---
+++ Day_022/pong/scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 80, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.set_scores()
+
+    def write_score(self, score):
+        self.write(score, align=ALIGNMENT, font=FONT)
+
+    def set_scores(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write_score(l_score)
+        self.goto(100, 200)
+        self.write_score(r_score)
+
+    def l_point(self):
+        self.set_scores()
+        self.l_score += 1
+
+    def r_point(self):
+        self.set_scores()
+        self.r_score += 1
