227dc5a9f7f2054bbb1254857602d80055a5d372
N-bred <thehardyboiz1@gmail.com>
2021-02-26 16:59:46 -0500

    Added paddles

--- Day_022/pong/main.py
+++ Day_022/pong/main.py
@@ -1,4 +1,6 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
+
 
 WIDTH, HEIGHT = 800, 600
 
@@ -6,6 +8,23 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=WIDTH, height=HEIGHT)
 screen.title("PONG")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
