843458ba758ed93102a866baa2888e4cdf440bf5
N-bred <thehardyboiz1@gmail.com>
2021-02-26 17:29:17 -0500

    Made logic of ball and bounces

--- Day_022/pong/main.py
+++ Day_022/pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
+from ball import Ball
+import time
 
 WIDTH, HEIGHT = 800, 600
 
@@ -13,7 +14,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -24,7 +25,26 @@ screen.onkeypress(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.04)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if (
+        ball.distance(r_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(l_paddle) < 50
+        and ball.xcor() < -320
+    ):
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 screen.exitonclick()
