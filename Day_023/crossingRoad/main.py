4fd0ad44bc60bac32b349683dd2715947f75337d
N-bred <thehardyboiz1@gmail.com>
2021-02-27 02:53:42 -0500

    Player can now finish the game

--- Day_023/crossingRoad/main.py
+++ Day_023/crossingRoad/main.py
@@ -16,7 +16,7 @@ player = Player()
 car_manager = CarManager()
 
 screen.listen()
-screen.onkey(player.go_up, "w")
+screen.onkeypress(player.go_up, "w")
 
 game_is_on = True
 while game_is_on:
@@ -24,5 +24,13 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
 
 screen.exitonclick()
