19bacb07c054f0461e2bb1e74730a7d1ba28f62a
N-bred <thehardyboiz1@gmail.com>
2021-03-13 23:06:32 -0500

    Small Change

--- Day_034/GUIQuizApp/data.py
+++ Day_034/GUIQuizApp/data.py
@@ -3,4 +3,4 @@ from api import get_questions
 
 URL = "https://opentdb.com/api.php"
 
-question_data = get_questions(URL, amount=10, type="boolean")
+question_data = get_questions(URL, amount=9, type="boolean")
