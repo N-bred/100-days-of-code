3bd363ef92df7f8ed0ede7dc5bc67e57ab5db564
N-bred <thehardyboiz1@gmail.com>
2021-03-24 04:02:25 -0500

    Basic canvas instantiation with styles

---
+++ Day_039/musicVisualizer/js/app.js
@@ -0,0 +1,13 @@
+const WIDTH = 600
+const HEIGHT = 480
+
+function setup() {
+  const cnv = createCanvas(600, 480)
+  cnv.parent('canvasParent')
+}
+
+function draw() {
+  background('#000')
+
+  noLoop()
+}
