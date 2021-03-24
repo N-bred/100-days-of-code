0eabec150d4a0a9fa99e8f8226bb80f20c3405c7
N-bred <thehardyboiz1@gmail.com>
2021-03-24 04:20:37 -0500

    Playing sounds with p5 sound

--- Day_039/musicVisualizer/js/app.js
+++ Day_039/musicVisualizer/js/app.js
@@ -1,9 +1,25 @@
 const WIDTH = 600
 const HEIGHT = 480
+let song
+
+function preload() {
+  soundFormats('mp3')
+  song = loadSound('/music/losing')
+  song.setVolume(0.1)
+}
 
 function setup() {
   const cnv = createCanvas(600, 480)
   cnv.parent('canvasParent')
+  cnv.mousePressed(playAudio)
+}
+
+function playAudio() {
+  if (!song.isPlaying()) {
+    song.play()
+  } else {
+    song.pause()
+  }
 }
 
 function draw() {
