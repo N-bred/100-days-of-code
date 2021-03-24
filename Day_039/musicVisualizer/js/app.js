d3ca33c6094c4c68c3780e1cc558d9fdd4e1c174
N-bred <thehardyboiz1@gmail.com>
2021-03-24 05:34:49 -0500

    Visualizer working with circles and rects

--- Day_039/musicVisualizer/js/app.js
+++ Day_039/musicVisualizer/js/app.js
@@ -1,6 +1,9 @@
-const WIDTH = 600
-const HEIGHT = 480
+const WIDTH = 1270
+const HEIGHT = 720
+const barWidth = 5
+const margin = 10
 let song
+let fft
 
 function preload() {
   soundFormats('mp3')
@@ -10,16 +13,42 @@ function preload() {
 
 function setup() {
   // Canvas
-  const cnv = createCanvas(600, 480)
+  const cnv = createCanvas(WIDTH, HEIGHT)
   cnv.parent('canvasParent')
   cnv.mousePressed(() => playAudio(song))
+
   // Volume Slider
   const slider = createVolumeSlider()
   slider.input(() => changeVolume(song, slider))
+  // FFT
+  fft = new p5.FFT()
+  fft.setInput(song)
+  background('#000')
+}
+function randomColor() {
+  return color(random(0, 255), random(0, 255), random(0, 255), random(50, 255))
+}
+
+function drawCircleRects(bars, color) {
+  translate(width / 2, height / 2)
+  fill(color)
+  bars.forEach((bar, i) => {
+    rotate(i)
+    rect(0, margin, barWidth, (bar * 2) % height)
+  })
+}
+
+function drawRectRects(bars, color) {
+  translate(0, height)
+  fill(color)
+  bars.forEach((bar, i) => {
+    rect(i * barWidth, 0, barWidth, (-bar * 2) % height)
+  })
 }
 
 function draw() {
-  background('#000')
+  //   clear()
 
-  noLoop()
+  drawCircleRects(fft.analyze(), randomColor())
+  //   drawRectRects(fft.analyze(), randomColor())
 }
