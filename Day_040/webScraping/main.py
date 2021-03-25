8f0669cf605fa95e18db11b6ed83c4c504048bec
N-bred <thehardyboiz1@gmail.com>
2021-03-25 06:30:45 -0500

    Added get info function to format data

--- Day_040/webScraping/main.py
+++ Day_040/webScraping/main.py
@@ -6,3 +6,10 @@ def make_request(url, params={}):
     req = requests.get(url)
     req.raise_for_status()
     return req.content
+
+
+def get_info(element):
+    return {
+        "id": element["id"].split("_")[1],
+        "score": int(element.text.split(" ")[0])
+    }
