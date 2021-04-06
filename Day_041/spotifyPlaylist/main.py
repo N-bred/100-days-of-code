b91be05bea3d396e68e57b9334e00f1b0e635ae9
N-bred <thehardyboiz1@gmail.com>
2021-04-06 16:44:38 -0500

    Integrated the scrapping with the spotify API to search for songs

--- Day_041/spotifyPlaylist/main.py
+++ Day_041/spotifyPlaylist/main.py
@@ -3,7 +3,9 @@ import requests
 from os import environ
 from dotenv import load_dotenv
 import spotipy
+from spotipy.client import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+import pprint
 
 scope = "playlist-modify-private"
 URL = "https://www.billboard.com/charts/hot-100/"
@@ -32,18 +34,39 @@ def get_100_billboard(date):
         "span", class_="chart-element__information")]
 
 
+def format_spotify_response(item):
+    return {
+        "id": item['id'],
+        "name": item["name"],
+        "href": item["href"],
+        "uri": item["uri"],
+        "preview_url": item["preview_url"],
+        "artists": [{"artist_name": i["name"], "artist_url": i["href"]} for i in item["artists"]],
+        "album": item["album"]["name"],
+        "images": [image for image in item["album"]["images"]]
+    }
+
+
+def make_spotify_request(sp: Spotify, query, reqType, name):
+    result = [item for item in sp.search(q=query)[
+        "tracks"]["items"] if item["type"] == reqType and item["name"].lower() == name.lower()]
+    if (len(result) == 0):
+        return f"query: {query} and name: {name} not found"
+    return format_spotify_response(result[0])
+
+
 def main():
 
     # date = input("Which year would you like to travel? (YYYY-MM-DD): ")
-    # if (date.count("-") > 3):
-    #     return print("Invalid Date")
+    date = "2000-08-15"
+    if (date.count("-") > 3):
+        return print("Invalid Date")
     load_dotenv()
-    # billboard = get_100_billboard(date)
-    # print(billboard)
-
+    billboard = get_100_billboard(date)[:5]
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    results = sp.current_user()
-    print(results)
+    songs = [make_spotify_request(sp,  (song["song_name"] + " " +
+                                        song["artist_name"]).replace(' ', '+'), "track", song["song_name"]) for song in billboard]
+    pprint.pprint(songs)
 
 
 if __name__ == '__main__':
